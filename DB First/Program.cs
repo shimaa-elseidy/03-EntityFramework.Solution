f7453a1f68310eca6e758f298feabbc156abeb62
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-02-24 23:05:12 +0200

    Mapping View

--- DB First/Program.cs
+++ DB First/Program.cs
@@ -22,7 +22,7 @@ using AppDbContext context = new AppDbContext();
 // 2. DML Statements [ update - insert - delete ]: ExecuteSqlRaw() - ExecuteSqlInterpolated()
 //var result = context.Categories.FromSqlRaw("Select * From Categories");
 //var result = context.Products.FromSqlRaw("Select * From Products where UnitsInStock = 0");
-int Price = 10;
+//int Price = 10;
 //var result = context.Products.FromSqlRaw($"Select * From Products where UnitPrice > {Price}");
 //var result = context.Products.FromSqlRaw("Select * From Products where UnitPrice > {0} ",Price);
 //var result = context.Products.FromSqlInterpolated($"Select * From Products where UnitPrice > {Price}");
@@ -30,4 +30,5 @@ int Price = 10;
 //{
 //    Console.WriteLine(item.ProductName);
 //}
-context.Database.ExecuteSqlRaw("Update Products Set UnitsInStock = 90 where ProductID = 1");
+//context.Database.ExecuteSqlRaw("Update Products Set UnitsInStock = 90 where ProductID = 1");
+//context.Database.ExecuteSqlRaw("Delete From Products where ProductID = 1");
